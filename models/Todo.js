f3260a1ec52d6dec1672990696e5cc98ffb95355
Олексій Лісковий <alyoshaliskovyi@gmail.com>
2025-12-08 18:44:04 +0000

    Upload New File

---
+++ models/Todo.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, { versionKey: false });
+
+export const Todo = mongoose.model("Todo", todoSchema);
